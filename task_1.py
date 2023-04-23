2e64302306d3752839c3910be63333da121f9866
LeonoraZH <102906152+LeonoraZH@users.noreply.github.com>
2023-04-23 14:04:56 +0300

    Add files via upload

--- task_1.py
+++ task_1.py
@@ -12,3 +12,24 @@
 --- '\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0430' - набор кодовых точек
 --- используйте списки и циклы, не дублируйте функции
 """
+
+word1 = 'разработка'
+word2 = 'сокет'
+word3 = 'декоратор'
+
+print(f"Тип переменной word1: {type(word1)}, содержание: {word1}")
+print(f"Тип переменной word2: {type(word2)}, содержание: {word2}")
+print(f"Тип переменной word3: {type(word3)}, содержание: {word3}")
+
+word1_unicode = [ord(letter) for letter in word1]
+word2_unicode = [ord(letter) for letter in word2]
+word3_unicode = [ord(letter) for letter in word3]
+
+word1_unicode = ''.join([f"\\u{ord(letter):04x}" for letter in word1])
+word2_unicode = ''.join([f"\\u{ord(letter):04x}" for letter in word2])
+word3_unicode = ''.join([f"\\u{ord(letter):04x}" for letter in word3])
+
+print(f"Тип переменной word1_unicode: {type(word1_unicode)}, содержание: {word1_unicode}")
+print(f"Тип переменной word2_unicode: {type(word2_unicode)}, содержание: {word2_unicode}")
+print(f"Тип переменной word3_unicode: {type(word3_unicode)}, содержание: {word3_unicode}")
+
--- task_2.py
+++ task_2.py
@@ -10,3 +10,18 @@
 --- b'class' - используйте маркировку b''
 --- используйте списки и циклы, не дублируйте функции
 """
+words = ['class', 'function', 'method']
+
+# Создаем пустой список, куда будем добавлять байтовые строки
+bytes_list = []
+
+# Проходимся по каждому слову в списке и преобразуем его в байтовую строку
+for word in words:
+    byte_word = b''
+    for letter in word:
+        byte_word += bytes(letter, encoding='utf-8')
+    bytes_list.append(byte_word)
+
+# Выводим тип, содержание и длину каждой переменной
+for i, byte_word in enumerate(bytes_list):
+    print(f"Тип переменной words[{i}]: {type(byte_word)}, содержание: {byte_word}, длина: {len(byte_word)}")
